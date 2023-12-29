fd689e42724c8329d03e43f8c1c1136e7aa38c85
0x235 <134880008+0x235z@users.noreply.github.com>
2023-12-29 01:18:41 -0300

    Update anti-fling.lua

--- anti-fling.lua
+++ anti-fling.lua
@@ -2,37 +2,40 @@
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 
--- fling players
-local function added(player : Player)
-	local character = player.Character
+local function get_player_fling(character)
+	local assembly_angular = character.PrimaryPart.AssemblyAngularVelocity
+	local assembly_linear = character.PrimaryPart.AssemblyLinearVelocity
 
-	player.CharacterAdded:Connect(function(new) 
-		character = new
-		while not character do
-			character = new
+	if assembly_angular.Magnitude > 50 or assembly_linear.Magnitude > 100 then
+		for i, v in character:GetDescendants() do
+			if v:IsA("BasePart") then
+				v.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+				v.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+				v.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0)
+			end
+		end
+		
+		character.PrimaryPart.CanCollide = false
+		character.PrimaryPart.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0)
+		assembly_angular = Vector3.new(0, 0, 0)
+		assembly_linear = Vector3.new(0, 0, 0)
+	end
+end
+
+local function added(player)
+	player.CharacterAdded:Connect(function(character)
+		while character do
+			get_player_fling(character)
 			task.wait()
 		end
 	end)
-
-	RunService.Heartbeat:Connect(function() 
-		if character and (character.PrimaryPart and character.PrimaryPart:IsDescendantOf(character)) then
-			if character.PrimaryPart.AssemblyAngularVelocity.Magnitude > 50 or character.PrimaryPart.AssemblyLinearVelocity.Magnitude > 100 then
-				for i, v in character:GetDescendants() do
-					if v:IsA("BasePart") then
-						v.CanCollide = false
-						v.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
-						v.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
-						v.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0)
-					end
-				end
-
-				character.PrimaryPart.CanCollide = false
-				character.PrimaryPart.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
-				character.PrimaryPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
-				character.PrimaryPart.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0)
-			end
+	
+	if player.Character then
+		while player.Character do
+			get_player_fling(player.Character)
+			task.wait()
 		end
-	end)
+	end
 end
 
 -- init
@@ -45,21 +48,22 @@ end
 
 Players.PlayerAdded:Connect(added)
 
--- fling client
 local last
-local character = player.Character
-player.CharacterAdded:Connect(function(c) character = c end)
-RunService.Heartbeat:Connect(function()
-	pcall(function()
-		local primary = character and character.PrimaryPart
+local function fling_player(character : Model)
+	local primary = character and character.PrimaryPart
+	
+	if primary and primary.AssemblyLinearVelocity.Magnitude > 250 or primary.AssemblyAngularVelocity.Magnitude > 250 then
+		primary.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+		primary.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
+		character:SetPrimaryPartCFrame(last)
+	else
+		last = character.PrimaryPart.CFrame
+	end
+end
 
-		if primary and primary.AssemblyLinearVelocity.Magnitude > 250 or primary.AssemblyAngularVelocity.Magnitude > 250 then
-			primary.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
-			primary.AssemblyAngularVelocity = Vector3.new(0, 0, 0)
-			character:SetPrimaryPartCFrame(last)
-			print("fling coco neutralizado")
-		elseif primary.AssemblyLinearVelocity.Magnitude < 50 or primary.AssemblyAngularVelocity.Magnitude > 50 then
-			last = primary.CFrame
-		end
-	end)
+player.CharacterAdded:Connect(function(character : Model)
+	while true do
+		pcall(fling_player, character)
+		task.wait()
+	end
 end)
